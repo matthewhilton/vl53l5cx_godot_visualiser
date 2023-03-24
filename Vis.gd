2ef00b8ffb9306e4fb0e451155cb6832e7abeeba
Matthew Hilton <mj.hilton@outlook.com>
2023-03-24 21:33:55 +1000

    Add dual sensor support

--- Vis.gd
+++ Vis.gd
@@ -1,7 +1,9 @@
 extends Spatial
 
 onready var serialNode = $Serial
-onready var visParent = $"Vis Parent"
+
+onready var sensorA = $"Sensor A"
+onready var sensorB = $"Sensor B"
 
 var fov_deg = 43
 var grid_size = 4
@@ -10,37 +12,48 @@ func _process(delta):
 	var result = JSON.parse(serialNode.data)
 	
 	if(result.error == OK):
-		var distances = result.result.distance
-		var status = result.result.status
+		process_sensor_data(result.result.a, sensorA, Color(0,1,0))
+		process_sensor_data(result.result.b, sensorB, Color(0,0,1))
 		
-		assert(distances.size() == grid_size * grid_size)
-		
-		# Drop all visualiser children.
-		for node in visParent.get_children():
-			visParent.remove_child(node)
-			node.queue_free()
-		
-		for row in range(0, grid_size):
-			for col in range(0, grid_size):
-				
-				var this_distance = distances[row * grid_size + col] / 50
-				var node = create_cube(Color(1,0,0), Vector3(1,1,1))
-				
-				visParent.add_child(node)
-				
-				var half_fov = fov_deg / 2
-				var fov_per_step = fov_deg / grid_size
-				
-				# Left/right angle, based on col
-				var angle_z = (-1 * half_fov) + (col * fov_per_step)
-				
-				# Up/down angle, based on row.
-				var angle_x = (-1 * half_fov) + (row * fov_per_step)
-				
-				node.rotate(node.transform.basis.z, deg2rad(angle_z))
-				node.rotate(node.transform.basis.x, deg2rad(angle_x))
-				node.translate(Vector3(0,this_distance,0))
-				
+
+func process_sensor_data(dataFromSensor, sensorNode, color):
+	var distances = dataFromSensor.distance
+	var status = dataFromSensor.status
+	
+	assert(distances.size() == grid_size * grid_size)
+	
+	# Drop all visualiser children.
+	for node in sensorNode.get_children():
+		# Skip the sensor body itself
+		if node is KinematicBody:
+			continue
+			
+		sensorNode.remove_child(node)
+		node.queue_free()
+	
+	for row in range(0, grid_size):
+		for col in range(0, grid_size):
+			
+			var this_distance = distances[row * grid_size + col] / 50
+			var node = create_cube(color, Vector3(1,1,1))
+			node.transform = sensorNode.transform
+			
+			sensorNode.add_child(node)
+			
+			var half_fov = fov_deg / 2
+			var fov_per_step = fov_deg / grid_size
+			
+			# Left/right angle, based on col
+			var angle_z = (-1 * half_fov) + (col * fov_per_step)
+			
+			# Up/down angle, based on row.
+			var angle_x = (-1 * half_fov) + (row * fov_per_step)
+			
+			node.global_rotate(node.global_transform.basis.z, deg2rad(angle_z))
+			node.global_rotate(node.global_transform.basis.x, deg2rad(angle_x))
+			
+			var movement = node.global_transform.basis.y * this_distance
+			node.global_translate(movement)
 
 func create_cube(color = Color(1,1,1), size = Vector3(0.2,0.2,0.2)):
 	var cube_mesh = CubeMesh.new()
--- code.py
+++ code.py
@@ -2,13 +2,17 @@ import busio
 from microcontroller import pin
 from digitalio import DigitalInOut, Direction
 import json
+import time
 
 from vl53l5cx import DATA_TARGET_STATUS, DATA_DISTANCE_MM
 from vl53l5cx import STATUS_VALID, RESOLUTION_4X4
 from vl53l5cx.cp import VL53L5CXCP
 
-scl_pin, sda_pin, lpn_pin, _ = (pin.GPIO1, pin.GPIO0, pin.GPIO28, pin.GPIO5)
+scl_pin, sda_pin, lpn_pin = (pin.GPIO1, pin.GPIO0, pin.GPIO28)
 i2c = busio.I2C(scl_pin, sda_pin, frequency=1_000_000)
+
+scl2_pin, sda2_pin, lpn2_pin = (pin.GPIO3, pin.GPIO2, pin.GPIO27)
+i2c2 = busio.I2C(scl2_pin, sda2_pin, frequency=1_000_000)
 
 uart = busio.UART(pin.GPIO12, pin.GPIO13, baudrate=9600)
 
@@ -16,33 +20,52 @@ lpn = DigitalInOut(lpn_pin)
 lpn.direction = Direction.OUTPUT
 lpn.value = True
 
+lpn2 = DigitalInOut(lpn2_pin)
+lpn2.direction = Direction.OUTPUT
+lpn2.value = True
+
 tof = VL53L5CXCP(i2c, lpn=lpn)
+tof2 = VL53L5CXCP(i2c2, lpn=lpn2)
+
+grid = 3
 
 tof.init()
 tof.resolution = RESOLUTION_4X4
-grid = 3
-
-tof.ranging_freq = 10
-
+tof.ranging_freq = 100
 tof.start_ranging({DATA_DISTANCE_MM, DATA_TARGET_STATUS})
 
+tof2.init()
+tof2.resolution = RESOLUTION_4X4
+tof2.ranging_freq = 100
+tof2.start_ranging({DATA_DISTANCE_MM, DATA_TARGET_STATUS})
+
+def get_tof_data(sensor):
+    while not sensor.check_data_ready():
+        time.sleep(0.01)
+    
+    results = sensor.get_ranging_data()
+    distance = results.distance_mm
+    status = results.target_status
+    
+    status_is_valid = list(map(lambda x: x == STATUS_VALID, status))
+        
+    data = {
+        'distance': distance,
+        'status': status_is_valid
+    }
+    
+    return data
+
 while True:
-    if tof.check_data_ready():
-        results = tof.get_ranging_data()
-        distance = results.distance_mm
-        status = results.target_status
+    tof1data = get_tof_data(tof)
+    tof2data = get_tof_data(tof2)
+    
+    data = {
+        'a': tof1data,
+        'b': tof2data
+    }
+    output = json.dumps(data)
         
-        status_is_valid = list(map(lambda x: x == STATUS_VALID, status))
-        
-        data = {
-            'distance': distance,
-            'status': status_is_valid
-        }
-        
-        output = json.dumps(data)
-        
-        #print(output)
-        #uart.write(bytes("test" + "\r\n", 'ascii'))
-        print(output)
-        uart.write(bytes(output + "\r\n", 'ascii'))
+    print(output)
+    uart.write(bytes(output + "\r\n", 'ascii')) 
 
